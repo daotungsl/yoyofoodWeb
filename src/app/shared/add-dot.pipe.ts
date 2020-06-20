208524c25a4e9790220adc03982bc0703e4a357a
daotungsl <daotungsl@gmail.com>
2020-06-20 10:02:38 +0700

    done

--- src/app/shared/add-dot.pipe.ts
+++ src/app/shared/add-dot.pipe.ts
@@ -7,7 +7,7 @@ export class AddDot implements PipeTransform {
    transform(num) {
     return (
       num
-        .toFixed(2) // always two decimal digits
+        .toFixed(0) // always two decimal digits
         .replace('.', ',') // replace decimal point character with ,
         .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') + 'đ'
     ) // use . as a separator
